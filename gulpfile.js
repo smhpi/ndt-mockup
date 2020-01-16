65bb0f8c484a491a07367420c5307ce98d0e3d88
Hosen Poursaeid <poursaeid@outlook.com>
2020-01-16 15:50:32 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    purgecss = require('gulp-purgecss'),
+    browserSync = require('browser-sync').create();
+
+    function css() {
+        return gulp.src('./src/assets/css/main.scss')
+          .pipe(sass().on('error', sass.logError))
+          .pipe(autoprefixer())
+          .pipe(cleanCSS({compatibility: 'ie8'}))
+          .pipe(rename(function(path) {
+            path.extname = ".min.css";
+          }))
+          .pipe(
+            purgecss({
+              content: ['public/**/*.html']
+            })
+          )
+          .pipe(gulp.dest('./public/assets/css/'))      
+          .pipe(browserSync.stream());
+      }
+    
+    function html() {
+        return gulp.src('./src/**/*.html')
+          .pipe(gulp.dest('./public/'));
+    }
+
+    function serve() {
+        browserSync.init({
+          server: {
+            baseDir: './public'
+          }
+        })
+      }
+
+      gulp.watch('./src/assets/css/**/*.scss', css);
+      gulp.watch('./src/*.html', html).on('change', browserSync.reload);
+      
+      exports.default = gulp.parallel(html, css, serve);
+
